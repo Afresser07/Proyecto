03357c8be3439385097425d99531d43a8b29e67f
Linda Ramirez <lramirezd2@ucentral.edu.co>
2024-04-16 23:24:25 -0500

    Estableciendo nombre archivos

---
+++ src/main/java/co/ucentral/sistema/Proyecto_Estudiantes/Proyecto_EstudiantesApplication.java
@@ -0,0 +1,15 @@
+package co.ucentral.sistema.Proyecto_Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto_EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto_EstudiantesApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+	
+}
