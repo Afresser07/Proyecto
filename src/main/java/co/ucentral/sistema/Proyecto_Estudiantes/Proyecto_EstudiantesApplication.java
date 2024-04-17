1ff6eb622e914d8cd27cab88de7cb0693b9212e0
Linda Ramirez <lramirezd2@ucentral.edu.co>
2024-04-16 23:29:51 -0500

    cambio nombres

---
+++ src/main/java/co/ucentral/sistema/Proyecto_Estudiantes/Proyecto_EstudiantesApplication.java
@@ -0,0 +1,14 @@
+package co.ucentral.sistema.Proyecto_Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto_EstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto_EstudiantesApplication.class, args);
+	}
+
+	
+}
