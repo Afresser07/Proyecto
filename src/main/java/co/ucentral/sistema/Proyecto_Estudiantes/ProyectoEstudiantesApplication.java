41a94d10cbcabd0ba0c2a4932c5bc3c512dea155
Linda Ramirez <lramirezd2@ucentral.edu.co>
2024-04-16 22:57:29 -0500

    Establecer archivos

---
+++ src/main/java/co/ucentral/sistema/Proyecto_Estudiantes/ProyectoEstudiantesApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistema.Proyecto_Estudiantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoEstudiantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoEstudiantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/sistema/Proyecto_Estudiantes/ProyectoEstudiantesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.sistema.Proyecto_Estudiantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoEstudiantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
