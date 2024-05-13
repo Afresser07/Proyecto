9e32f28b11b83c1b308d0c16a58c50f25fb0bccd
Afresser07 <javilac8@ucentral.edu.co>
2024-05-13 00:09:28 -0500

    Adelanto caso de uso 6

---
+++ src/main/java/co/ucentral/sistema/Proyecto_Estudiantes/entidades/Asignatura.java
@@ -0,0 +1,10 @@
+package co.ucentral.sistema.Proyecto_Estudiantes.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name= "asignaturas")
+public class Asignatura {
+    
+}
