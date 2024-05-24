f89fef932d6cc88af6a51393c3745cce69c3b28b
LinMichRa <lramirezd2@ucentral.edu.co>
2024-05-24 14:15:03 -0500

    Implementacion Notal Final Cortes

--- src/main/java/co/ucentral/sistema/Proyecto_Estudiantes/controladores/ControladorCalificacion.java
+++ src/main/java/co/ucentral/sistema/Proyecto_Estudiantes/controladores/ControladorCalificacion.java
@@ -1,6 +1,8 @@
 package co.ucentral.sistema.Proyecto_Estudiantes.controladores;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -49,8 +51,11 @@ public class ControladorCalificacion {
     public String verCalificacionesPorCorte(@PathVariable Integer asignaturaId, @RequestParam Integer corteId, Model model) {
         Asignatura asignatura = operacionesAsignatura.findByCodigo(asignaturaId);
         List<Calificacion> calificaciones = repositorioCalificacion.findByAsignaturaIdAndCorteId(asignaturaId, corteId);
+        Set<Estudiante> estudiante = new HashSet<>(operacionesEstudiante.findByAsignaturasCodigo(asignaturaId));
         model.addAttribute("asignatura", asignatura);
         model.addAttribute("calificaciones", calificaciones);
+        model.addAttribute("estudiante", estudiante);
+        model.addAttribute("operaciones", operacionesCalificacion);
         return "visualizarCalificaciones";
     }
 
@@ -58,26 +63,49 @@ public class ControladorCalificacion {
     public String mostrarFormularioCalificacion(@RequestParam Integer asignaturaId, Model model) {
         List<Actividad> actividades = operacionesActividad.findByAsignaturaId(asignaturaId);
         List<Estudiante> estudiantes = operacionesEstudiante.findByAsignaturasCodigo(asignaturaId);
+        
         model.addAttribute("actividades", actividades);
         model.addAttribute("estudiantes", estudiantes);
+        
         model.addAttribute("asignaturaId", asignaturaId);
+        
         return "registroCalificaciones";
     }
 
     @PostMapping("/CrearCalificacion")
-    public String guardarCalificacion(@RequestParam Integer actividadId, @RequestParam Integer estudianteId, @RequestParam Integer valor, @RequestParam Integer asignaturaId) {
+    public String guardarCalificacion(@RequestParam Integer actividadId, @RequestParam Integer estudianteId, @RequestParam Integer asignaturaId, @RequestParam Integer nota, Model modelo) {
         Actividad actividad = operacionesActividad.findByCodigo(actividadId);
         Estudiante estudiante = operacionesEstudiante.findByCedula(estudianteId);
         Asignatura asignatura = operacionesAsignatura.findByCodigo(asignaturaId);
 
+        if (actividad == null) {
+            modelo.addAttribute("error", "La actividad no existe.");
+            return "registroCalificaciones";
+        }
+        if (estudiante == null) {
+            modelo.addAttribute("error", "El estudiante no existe.");
+            return "registroCalificaciones";
+        }
+        if (asignatura == null) {
+            modelo.addAttribute("error", "La asignatura no existe.");
+            return "registroCalificaciones";
+        }
+
+        if (nota > actividad.getPuntos()) {
+            modelo.addAttribute("error", "Los puntos perdidos no pueden exceder los puntos máximos de la actividad.");
+            return "registroCalificaciones";
+        }
+
+        
+
         Calificacion calificacion = new Calificacion();
         calificacion.setActividad(actividad);
         calificacion.setEstudiante(estudiante);
-        calificacion.setAsignaturaId(asignatura);
-        calificacion.setNota(valor);
-        
-        operacionesCalificacion.guardarCalificacion(calificacion);
-        return "redirect:/AsignaturasProfesor";
+        calificacion.setNota(nota);
 
+        operacionesCalificacion.guardarCalificacion(calificacion);
+
+        return "redirect:/AsignaturasProfesor";
     }
+
 }
